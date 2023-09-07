fb1f043ac4f35b577c92f73c64ee7c8d949dcac1
Lakshay <83589511+TESLA2402@users.noreply.github.com>
2023-09-07 12:31:46 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Lakshay",
+  title: "Developer",
+  email: "lakshay6447@gmail.com",
+  gitHub: "TESLA2402",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
+  twitter: "",
   youTube: "Code",
 };
 
